239352b354ffc7b0435f03399194088ff9d6511b
simonemayers <simone21295@gmail.com>
2020-10-07 16:51:09 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length >= 3
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    elsif array.size == 2 
+        array.join(" and ")
+    else
+        array.join
+    end
 end
